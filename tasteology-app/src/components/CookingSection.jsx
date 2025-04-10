83656e14d9d3259d0eefb378bd9053cb0e3c94f6
geneparagas <gene.paragas0408@gmail.com>
2025-04-10 22:21:57 +0800

    what does cooking component

---
+++ tasteology-app/src/components/CookingSection.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const CookingSection = ({ data, onImageClick, onAnchorClick }) => (
+  <section className="cooking-section fade-in">
+    <div className="wrapper">
+      {data.images.map((img, i) => (
+        <div className={`image ${img.class}`} key={i}>
+          <img
+            src={img.src}
+            alt={img.alt}
+            className="clickable"
+          />
+        </div>
+      ))}
+      <div className="content">
+        <h2 className="title">{data.title}</h2>
+        <hr />
+        <p>{data.description}</p>
+        <a onClick={onAnchorClick}>{data.ctaEgg}</a>
+        <h3>{data.cta}</h3>
+      </div>
+    </div>
+  </section>
+);
+
+export default CookingSection;
